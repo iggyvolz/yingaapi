d192844e568f832226a810accaa4999ce1cd312c
Katie Volz <iggyvolz@gmail.com>
2020-07-02 17:24:16 -0400

    Allow promoting parameters to properties

---
+++ tests/PromotionTest.php
@@ -0,0 +1,61 @@
+<?php
+declare(strict_types=1);
+namespace iggyvolz\yingaapi\tests;
+
+use Attribute;
+use ReflectionMethod;
+use PHPUnit\Framework\TestCase;
+use iggyvolz\yingaapi\ApiRunner;
+use iggyvolz\yingaapi\Annotations\Promoted;
+use iggyvolz\yingaapi\Annotations\ApiMethod;
+use iggyvolz\ClassProperties\ClassProperties;
+use iggyvolz\yingaapi\Annotations\ThisResolver;
+use iggyvolz\ClassProperties\Attributes\Property;
+use iggyvolz\yingaapi\DependencyInjection\DependencyInjectionContext;
+
+class PromotionTest extends TestCase
+{
+    private ApiRunner $runner;
+    private DependencyInjectionContext $context;
+    public function setUp():void
+    {
+        $this->runner = new ApiRunner([DummyType::class]);
+        $this->context = new DependencyInjectionContext;
+    }
+    public function testSetMyName():void
+    {
+        $result = $this->runner->run("SetMyName", ["name" => "Charlie"], $this->context)->getResponse();
+        $this->assertSame("Charlie", $result);
+    }
+    public function testSetMyNameAlt():void
+    {
+        $result = $this->runner->run("SetMyNameAlt", ["myName" => "Charlie"], $this->context)->getResponse();
+        $this->assertSame("Charlie", $result);
+    }
+}
+
+class DummyType extends ClassProperties
+{
+    <<Property>> private string $name;
+    <<ApiMethod("SetMyName")>>
+    <<DummyResolver>>
+    public function setMyName(<<Promoted>> string $name):string
+    {
+        return $this->name;
+    }
+    <<ApiMethod("SetMyNameAlt")>>
+    <<DummyResolver>>
+    public function setMyNameAlt(<<Promoted("name")>> string $myName):string
+    {
+        return $this->name;
+    }
+}
+
+<<Attribute(Attribute::TARGET_METHOD)>>
+class DummyResolver extends ThisResolver
+{
+    public function getThis(ReflectionMethod $method, array $data):object
+    {
+        return new DummyType;
+    }
+}
